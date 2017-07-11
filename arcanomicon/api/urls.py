f1402b39e0bd65f8d6596985e273173ae0a647c7
Retzudo <christoph.willinger@gmail.com>
2017-07-11 16:52:20 +0200

    Add-on API list

--- arcanomicon/api/urls.py
+++ arcanomicon/api/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 
 from api import views
 
+router = DefaultRouter()
+router.register(r'addons', views.AddOnViewSet, base_name='addons')
+
 urlpatterns = [
+    url(r'', include(router.urls)),
     url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'favourites/$', views.FavouritesView.as_view()),
     url(r'favourites/(?P<add_on_id>\d+)$', views.FavouritesView.as_view()),
--- arcanomicon/api/views.py
+++ arcanomicon/api/views.py
@@ -1,21 +1,43 @@
+from django.db.models import Q
+from rest_framework import permissions
+from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import GenericAPIView, ListAPIView, get_object_or_404
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api import serializers
 from core.models import AddOn
 
 
-class FavouritesView(ListAPIView, CreateModelMixin, DestroyModelMixin, GenericAPIView):
+class AddOnViewSet(viewsets.ModelViewSet):
+    """List all add-ons."""
     serializer_class = serializers.AddOnSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """Allow a `q` paramter for searching."""
+        query = self.request.query_params.get('q')
+
+        if query:
+            return AddOn.objects.filter(Q(name__contains=query.lower()) | Q(short_description__contains=query.lower()))
+        else:
+            return AddOn.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user.user)
+
+
+class FavouritesView(ListAPIView, CreateModelMixin, DestroyModelMixin, GenericAPIView):
+    """List all favourites for a user."""
+    serializer_class = serializers.AddOnSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         return self.request.user.user.favourites.all()
 
     def post(self, request):
+        """Add an existing add-on to the current user's favourites list."""
         add_on_id = request.data.get('addOnId')
         if not add_on_id:
             raise ValidationError('addOnId missing')
@@ -30,6 +52,7 @@ class FavouritesView(ListAPIView, CreateModelMixin, DestroyModelMixin, GenericAP
         return Response('', status=201)
 
     def delete(self, request, add_on_id):
+        """Remove an add-on from the current user's list."""
         try:
             add_on = request.user.user.favourites.get(pk=add_on_id)
         except AddOn.DoesNotExist:
@@ -41,13 +64,17 @@ class FavouritesView(ListAPIView, CreateModelMixin, DestroyModelMixin, GenericAP
 
 
 class DeleteAddOnVersion(DestroyModelMixin, GenericAPIView):
-    permission_classes = (IsAuthenticated,)
+    """Delete a version of an add-on.
+
+    Only the add-on's creator is allowed to delete versions."""
+    permission_classes = (permissions.IsAuthenticated,)
 
     def delete(self, request, add_on_id, version_id):
         add_on = get_object_or_404(AddOn, pk=add_on_id)
         version = add_on.versions.get(pk=version_id)
 
         if add_on.creator.pk != request.user.user.pk:
+            # Only the add-on's creator is allowed to delete versions.
             raise PermissionError()
 
         version.delete()
